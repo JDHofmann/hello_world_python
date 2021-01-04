aa87936ea9c1c1ef1b8fcd244493f0d6150df7bc
jdhofmann <justindhofmann@gmail.com>
2021-01-04 18:07:53 -0500

    object/hash

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
-msg = "Hello world"
-me = "jd "
-times = 23
-print(msg.split())
+# msg = "Hello world"
+# me = "jd "
+# times = 23
+# print(msg.split())
 
-print("*" * 10)
-print(me * times)
+# print("*" * 10)
+# print(me * times)
+
+john = {
+    "name": "john",
+    "age": 23
+}
+
+print(john["age"])

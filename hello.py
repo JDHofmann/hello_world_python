0a5bd116db5a30bba40618677338a9fe89382a5d
jdhofmann <justindhofmann@gmail.com>
2021-01-04 18:11:50 -0500

    data types

--- hello.py
+++ hello.py
@@ -6,9 +6,14 @@
 # print("*" * 10)
 # print(me * times)
 
-john = {
-    "name": "john",
-    "age": 23
-}
+# john = {
+#     "name": "john",
+#     "age": 23
+# }
 
-print(john["age"])
+# print(john["age"])
+
+birth_year = input("Birth year: ")
+print(type(birth_year))
+age = 2021 - int(birth_year)
+print(age)

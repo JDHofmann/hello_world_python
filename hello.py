32374736d6fce98eeb237ed91a2dd4f4ae4291f6
jdhofmann <justindhofmann@gmail.com>
2021-01-04 19:41:42 -0500

    strings!

--- hello.py
+++ hello.py
@@ -13,7 +13,20 @@
 
 # print(john["age"])
 
-birth_year = input("Birth year: ")
-print(type(birth_year))
-age = 2021 - int(birth_year)
-print(age)
+# birth_year = input("Birth year: ")
+# print(type(birth_year))
+# age = 2021 - int(birth_year)
+# print(age)
+
+# weight_lbs = input("Weight (lbs): ")
+# weight_kg = int(weight_lbs) * .45
+# print(weight_kg)
+
+# a = '''
+#     Hey bud,
+#     missed you last week,
+
+#     ok bye
+# '''
+# print(a)
+
